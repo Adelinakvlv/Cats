e7fcf6219a9fcb7ecd769fa0856c444ac4a01de2
Del1chka <kovalevadelya@mail.ru>
2024-09-09 22:39:28 +0300

    Заманили кнопку на меню с функцией выхода.

--- cats.py
+++ cats.py
@@ -5,6 +5,9 @@ from PIL import Image, ImageTk
 import requests
 from io import BytesIO
 
+from PIL.ImageWin import Window
+
+
 def load_image(url):
     try:
         response=requests.get(url)
@@ -24,6 +27,9 @@ def set_image():
         label.config(image=img)
         label.image = img
 
+def exit():
+    window.destroy()
+
 window=Tk()
 window.title("Cats!")
 window.geometry("600x520")
@@ -31,8 +37,14 @@ window.geometry("600x520")
 label=Label()
 label.pack()
 
-update_button=Button(text="Обновить!", command=set_image)
-update_button.pack()
+menu_bar=Menu(window)
+window.config(menu=menu_bar)
+
+file_menu=Menu(menu_bar, tearoff=0)
+menu_bar.add_cascade(label="Файл", menu=file_menu)
+file_menu.add_command(label="Загрузить фото", command=set_image)
+file_menu.add_separator()
+file_menu.add_command(label="Выход", command=exit)
 
 url="https://cataas.com/cat"
 
